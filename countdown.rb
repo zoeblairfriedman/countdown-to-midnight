afcf02af36abdbc88d62d325095cb10a75f3f60d
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-12 08:36:39 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+    while seconds > 0
+        puts "#{seconds} SECOND(S)!"
+        seconds -= 1
+    end
+        "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(seconds)
+    while seconds > 0
+        puts "#{seconds} SECOND(S)!"
+        seconds -= 1
+        sleep 1
+    end
+        "HAPPY NEW YEAR!"
 end
